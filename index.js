f3472bb55a4068ca9c93b1ec9a723aa654ce30eb
youtanimstar <animdeep2019@gmail.com>
2022-08-03 21:43:38 +0530

    new file added

---
+++ index.js
@@ -0,0 +1,7 @@
+const mobileNav = document.querySelector(".mobile-navbar-btn");
+const navHeader = document.querySelector(".header");
+
+const toggleNavbar = () =>{
+    navHeader.classList.toggle("active");
+}
+mobileNav.addEventListener('click', () => toggleNavbar());
